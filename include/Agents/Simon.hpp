230ad9fc26d56d786cc9f5bbb05dbe9ce49688b9
Simon Struck <simon@simonscode.org>
2019-06-05 00:30:53 +0200

    Add Agent Simon

---
+++ include/Agents/Simon.hpp
@@ -0,0 +1,45 @@
+/**
+*   \copyright Copyright 2016 Hochschule Emden/Leer. This project is released
+* under
+* the MIT License, see the file
+* LICENSE.md for rights and limitations.
+*   \file Hunter.hpp
+*   \author Jan-Niklas Braak
+*/
+
+#ifndef __SIMON__
+#define __SIMON__
+
+#include "Agent.hpp"
+#include "Robot.hpp"
+#include "Angles.hpp"
+
+#include "mathUtility.hpp"
+
+#include <iostream>
+/**
+  \brief An Agent that shoots the closes visible Robot.
+*/
+class Simon final : public Agent {
+  double targetDistance;
+  double K_perp;
+  double K_straight;
+
+public:
+  /**
+  Construct a Hunter.
+  */
+  Simon(double targetDistanc, double K_perp, double K_straight);
+
+  /**
+  During each time step the Hunter trys to get clother to the closes visible
+  Robot.
+  */
+  Action update(Robot const &r) override;
+
+private:
+  double turn_perpendicular(double ang, double rotation) const;
+  double turn_toward(double ang, double rotation) const;
+};
+
+#endif /* end of include guard: __SIMON__ */
---
+++ src/Agents/Simon.cpp
@@ -0,0 +1,96 @@
+/**
+*   \copyright Copyright 2016 Hochschule Emden/Leer. This project is released
+* under
+* the MIT License, see the file
+* LICENSE.md for rights and limitations.
+*   \file Hunter.cpp
+*   \author Jan-Niklas Braak
+*/
+#include <cmath>
+#include <iostream>
+
+#include "Agents/Simon.hpp"
+#include "Angles.hpp"
+#include "mathUtility.hpp"
+
+Simon::Simon(double targetDistance, double K_perp, double K_straight)
+  : targetDistance(targetDistance), K_perp(K_perp), K_straight(K_straight) {}
+
+Action Simon::update(Robot const &r) {
+  const auto target_robot = r.scanClosest();
+  const auto position = r.getPosition();
+  const auto rotation = r.getRotation();
+
+  /*
+    const auto target_position =
+        target_robot ?  target_robot->getPosition() : r.rules.arena_size / 2.0;
+
+    const auto delta_position = target_position - position;
+
+    const auto turret_angle = target_robot
+                                  ? delta_position.angle() - rotation :
+                                  r.getTurretAngle() + r.rules.scan_angle;
+  */
+
+  double turret_angle;
+  Vector_d delta_position;
+
+  if (!target_robot) {
+    // Set the center of the arena as target.
+    const auto target_position = r.rules.arena_size / 2.0;
+    delta_position = target_position - position;
+
+    // Rotate the Turret to scan for targets
+    turret_angle = r.getTurretAngle() + r.rules.scan_angle;
+  } else {
+    // Set the found Robot as Target.
+    const auto target_position = target_robot->getPosition();
+    delta_position = target_position - position;
+
+    // Aim at target
+    turret_angle = delta_position.angle() - rotation;
+  }
+
+  // calculate w for the two rules.
+  const auto w_toward = turn_toward(delta_position.angle(), rotation);
+  const auto w_perp = turn_perpendicular(delta_position.angle(), rotation);
+
+  // How far are we away from the target distance, as a value from -1 to 1.
+  const auto distance_error =
+    clamp(delta_position.magnitude() / targetDistance - 1.0, -1.0, 1.0);
+
+  /*
+   * Put it all together. Linearly interpolate between driving toward the target
+   * and driving perpendicular to it depending on the distance. So that at the
+   * target distance we drive fully perpendicular, if we are farher away we
+   * drive toward the target and if we are to close we drive away from the
+   * target (due to the negative sign of the distance error).
+   */
+  const auto w = lerp(w_perp, w_toward, distance_error);
+
+  // Is the target in front of the turret (within 0.01rad)? If yes then shoot.
+  const auto turret_error = angDiffRadians(turret_angle, r.getTurretAngle());
+
+  // drive at full speed.
+  const auto v = r.rules.v_max;
+
+  return {v, w, turret_angle, true};
+}
+
+double Simon::turn_perpendicular(double angle, double rotation) const {
+  // turn perpandicular to the target
+  const auto perp_error1 = angDiffRadians(angle + M_PI / 2, rotation);
+  const auto perp_error2 = angDiffRadians(angle - M_PI / 2, rotation);
+  const auto perp_error =
+    fabs(perp_error1) < fabs(perp_error2) ? perp_error1 : perp_error2;
+  const auto w = perp_error * K_perp;
+
+  return w;
+}
+
+double Simon::turn_toward(double angle, double rotation) const {
+  const auto straight_error = angDiffRadians(angle, rotation);
+  const auto w = straight_error * K_straight;
+
+  return w;
+}
